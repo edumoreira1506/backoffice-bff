860f8855bf69017775afed23b8120aae041730a2
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-01 23:05:16 -0300

    Remove code (#2)
    * Bump @cig-platform/core to 0.4.10
    
    * Remove code from project

--- src/middlewares/withTokenAuthoritzation.ts
+++ src/middlewares/withTokenAuthoritzation.ts
@@ -1,31 +1,6 @@
-import { NextFunction, Response } from 'express'
-import { AccountServiceClient, AuthError, BaseController } from '@cig-platform/core'
-import { ApiErrorType } from '@cig-platform/types'
+import { BaseController, withTokenAuthorizationFactory } from '@cig-platform/core'
 
 import TokenService from '@Services/TokenService'
 import AccountClient from '@Clients/AccoutnServiceClient'
-import { AuthenticatedRequest } from '@Types/request'
 
-export const withTokenAuthorizationFactory = (errorCallback: (res: Response, error: ApiErrorType) => Response, accountServiceClient: AccountServiceClient) => async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-  try {
-    const token = req.header('X-Cig-Token')
-
-    if (!token) throw new AuthError()
-
-    const tokenData = await TokenService.open(token)
-
-    if (!tokenData?.id) throw new AuthError()
-
-    const userData = await accountServiceClient.getUser(String(tokenData?.id))
-
-    if (userData?.id !== tokenData?.id) throw new AuthError()
-
-    req.user = userData
-
-    next()
-  } catch (error: any) {
-    return errorCallback(res, error?.getError ? error.getError() : error)
-  }
-}
-
-export default withTokenAuthorizationFactory(BaseController.errorResponse, AccountClient)
+export default withTokenAuthorizationFactory(TokenService, BaseController.errorResponse, AccountClient)
