0be38f5546067c8dd7c7ea1923f6f3ca1ea44381
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-01 23:02:22 -0300

    Remove code from project

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -1,20 +1,5 @@
-import jwt from 'jsonwebtoken'
-import { IBreeder, IUser } from '@cig-platform/types'
+import { TokenService } from '@cig-platform/core'
 
 import { JWT_ENCRYPT_SECRET } from '@Constants/jwt'
 
-export default class TokenService {
-  static async create({ email, id, name }: Required<IUser>, breeders: Required<IBreeder>[]): Promise<string> {
-    return jwt.sign({ email, id, name, breeders }, JWT_ENCRYPT_SECRET, { expiresIn: '1d' })
-  }
-
-  static open(token: string): Promise<Record<string, unknown>> {
-    return new Promise ((resolve, reject) => {
-      jwt.verify(token, JWT_ENCRYPT_SECRET, (error: any, decoded: any): void => {
-        if (error) reject(error)
-
-        return resolve(decoded)
-      })
-    })
-  }
-}
+export default new TokenService(JWT_ENCRYPT_SECRET)
