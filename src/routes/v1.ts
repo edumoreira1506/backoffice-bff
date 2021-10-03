35c0330fb78193a7c70902b5993dc7d01745db14
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-03 17:10:53 -0300

    Add profile image on edit breeder

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -3,9 +3,10 @@ import express from 'express'
 import withBreederPermission from '@Middlewares/withBreederPermission'
 import withTokenAuthorization from '@Middlewares/withTokenAuthoritzation'
 import BreederController from '@Controllers/BreederController'
+import withFileSupport from '@Middlewares/withFileSupport'
 
 const router = express.Router()
 
-router.patch('/breeders/:breederId', withTokenAuthorization, withBreederPermission, BreederController.update)
+router.patch('/breeders/:breederId', withTokenAuthorization, withBreederPermission, withFileSupport, BreederController.update)
 
 export default router
