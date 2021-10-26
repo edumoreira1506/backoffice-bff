d5fafa3a7b98f192053da217c1064363b289c5a8
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-25 21:23:07 -0300

    Add registers to getPoultry

--- src/aggregators/PoultryAggregator.ts
+++ src/aggregators/PoultryAggregator.ts
@@ -53,8 +53,9 @@ export class PoultryAggregator {
   async getPoultry(breederId: string, poultryId: string) {
     const poultry = await this._poultryServiceClient.getPoultry(breederId, poultryId)
     const images = await this._poultryServiceClient.getPoultryImages(breederId, poultryId)
+    const registers = await this._poultryServiceClient.getRegisters(breederId, poultryId)
 
-    return { ...poultry, images }
+    return { ...poultry, images, registers }
   }
 }
 
