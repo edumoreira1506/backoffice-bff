3583b7e8a2fcda8dcd3fd038efd69ad0fb983271
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-29 22:37:14 -0300

    Fix import

--- src/clients/AccountServiceClient.ts
+++ src/clients/AccountServiceClient.ts
@@ -1,5 +1,5 @@
 import { AccountServiceClient } from '@cig-platform/core'
 
-import { ACCOUNT_SERVICE_URL } from '@Constants/account'
+import { ACCOUNT_SERVICE_URL } from '@Constants/url'
 
 export default new AccountServiceClient(ACCOUNT_SERVICE_URL)
