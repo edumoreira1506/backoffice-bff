8165c88806beede455a307552d27f59c63f37b70
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-14 23:49:35 -0300

    Change remove advertising rules

--- src/aggregators/AdvertisingAggregator.ts
+++ src/aggregators/AdvertisingAggregator.ts
@@ -4,12 +4,13 @@ import {
   DealServiceClient as IDealServiceClient
 } from '@cig-platform/core'
 import { IAdvertising } from '@cig-platform/types'
-import { RegisterTypeEnum } from '@cig-platform/enums'
+import { DealEventValueEnum, RegisterTypeEnum } from '@cig-platform/enums'
 
 import AdvertisingServiceClient from '@Clients/AdvertisingServiceClient'
 import PoultryServiceClient from '@Clients/PoultryServiceClient'
 import DealServiceClient from '@Clients/DealServiceClient'
 import DealRunningError from '@Errors/DealRunningError'
+import DealAggregator from '@Aggregators/DealAggregator'
 
 export class AdvertisingAggregator {
   private _advertisingServiceClient: IAdvertisingServiceClient;
@@ -79,12 +80,21 @@ export class AdvertisingAggregator {
     poultryId: string,
   }) {
     const deals = await this._dealServiceClient.getDeals({ advertisingId })
-    const hasUnfinishedDeals = deals.some((deal) => !deal.finished && !deal.cancelled)
+    const dealEvents = await Promise.all(deals.map(async deal => this._dealServiceClient.getDealEvents(deal.id)))
+    const hasConfirmedDeals = dealEvents.some((events) =>
+      events.some(e => e.value === DealEventValueEnum.confirmed) &&
+      events.every(e => e.value !== DealEventValueEnum.cancelled) &&
+      events.every(e => e.value !== DealEventValueEnum.received)
+    )
 
-    if (hasUnfinishedDeals) throw new DealRunningError()
+    if (hasConfirmedDeals) throw new DealRunningError()
 
     const poultry = await this._poultryServiceClient.getPoultry(breederId, poultryId)
     const breeder = await this._poultryServiceClient.getBreeder(breederId)
+
+    deals.map(async deal => {
+      await DealAggregator.cancelDeal(deal.id, 'Anúncio cancelado')
+    })
 
     await this._advertisingServiceClient.removeAdvertising(merchantId, advertisingId)
     await this._poultryServiceClient.postRegister(
